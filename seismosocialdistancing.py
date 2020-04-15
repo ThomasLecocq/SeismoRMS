e4c32c1bfb194b79687bbb04613735c2b10678ee
Frédérick Massin <fmassin@sed.ethz.ch>
2020-04-15 12:39:02 +0200

    Add hour map and new module
    Following uo on the nice `clockplots` added to the main notebook by
    @ThomasLecocq and the growing complexity, I mean to start developing
    within a module, than can be then imported in notebooks.
    
    The module is `seismosocialdistancing.py`. I am proposing to put all
    our functions there and import the module in all notebooks instead of
    creating functions or long codes in notebook cells. In case it is not
    obvious, while developing a new function in module, one can reload the
    module with:
    ```
    import imp
    imp.reload(seismosocialdistancing)
    ```
    
    I demonstrate how to in sqlxSocialDistancing.ipynb with:
    - the sqlx2rms function which produce an object with PSDs and an dRMS
    method.
    - the hourmap function (poor name?) which makes a map of dRMS aroud the
    clock.

--- seismosocialdistancing.py
+++ seismosocialdistancing.py
@@ -1,7 +1,16 @@
-import subprocess,sys
 import pandas as pd
 import numpy as np
 from obspy import UTCDateTime
+
+# For sqlx
+import subprocess,sys
+
+# For hour map
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.patheffects as pe
+
 
 class PSDs(object):
     def __init__(self,count={},psd={},per={},times=[],mseedids=[]):
@@ -71,7 +80,25 @@ def sqlx2drms(sshuserhost,
               start = UTCDateTime()-3*24*60*60,#"2020-03-07")
               end = UTCDateTime(),# means "now"
               freqs = [(0.1,1.0),(1.0,20.0),(4.0,14.0),(4.0,20.0)]):
+    """
+    Get PSDs from PQLX
+    
+    :type sshuserhost: string.
+    :param sshuserhost: ssh connection string, e.g. login@hostname.
+    :type network,station,location,channel: string.
+    :param network,station,location,channel: the mseed codes, use ',' as separator to get several channels.
+    :type start, end: `obspy.UTCDateTime``.
+    :param start, end: time window.
+    :type freqs: list of tuples.
+    :param freqs: frequency ranges (one each tuple).
+    :return: `PSDs`object.
 
+    .. rubric:: Basic Usage
+
+    You may omit everyhting but sshuserhost.
+
+    >>>myPSDs = sqlx2drms('login@hostname')
+    """
     psds=PSDs()
     datesplit = pd.date_range(start.datetime, 
                               end.datetime, 
@@ -97,7 +124,7 @@ def sqlx2drms(sshuserhost,
         for date1 in datelist:
             if date1>= end.datetime:
                 break
-            date2 = date1+pd.Timedelta(30, unit='min')
+            date2 = date1+pd.Timedelta(minutes=30)
             for n in network.split(','):
                 for s in station.split(','):
                     for l in location.split(','):
@@ -130,3 +157,92 @@ def sqlx2drms(sshuserhost,
                 psds.psd[(mseedid,time)] += [float(data[3])]
                 psds.per[(mseedid,time)] += [float(data[2])]
     return psds
+    
+
+def hourmap(data,
+            bans=None,
+            ax=None,
+            scale = 1e9):
+    """
+    Make a polar plot of rms
+
+    :type data: dataframe.
+    :param data: the rms.
+    :type bans: dict.
+    :param bans: some annotation, keys are date strings, fields are text desc strings.
+    :type ax: axe.
+    :param ax: use the provided exiting axe if provided.
+    :type scale: float.
+    :param scale: scale amplitudes (to nm by default).
+    :return: A axe with the plot.
+
+    .. rubric:: Basic Usage
+
+    You may omit bans, ax and scale parameters.
+
+    >>> ax = hourmap(data[mseedid])
+    """
+    width = data.index[1]-data.index[0]
+    width = np.pi * 2 / 24 / 60 /60 * width.seconds 
+    theta = np.asarray([(d.hour/24+d.minute/60/24)*np.pi*2-width/2 for d in data.index])
+    radii = np.asarray([int(d.to_julian_date()+0.5) for d in data.index])
+    radii = radii-min(radii)
+    norm = colors.Normalize(vmin=np.nanpercentile(data,1),
+                            vmax=np.nanpercentile(data,95))
+    c_m = plt.cm.viridis
+    s_m = plt.cm.ScalarMappable(cmap=c_m, 
+                                norm=norm)
+    s_m.set_array([])
+    valid = np.where(np.isfinite(data))[0][::-1]
+    
+    if ax is None:
+        ax=plt.figure(figsize=(7,9)).add_subplot(111, projection='polar')
+    ax.grid(color='w',
+            #path_effects=[pe.withStroke(linewidth=2,foreground='w')]
+            )
+    ax.set_xticks(np.linspace(0,np.pi*2*23/24,24))
+    ax.set_xticklabels(['%dh'%h for h in range(24)])
+    ax.set_theta_zero_location("N")
+    ax.set_theta_direction(-1)
+    ax.set_rmax(max(radii))
+    if bans is not None:
+        rticks = [(UTCDateTime(ban).datetime - data.index.min().to_pydatetime()).days for iban,ban in enumerate(bans.keys())]
+        xticks = [(UTCDateTime(ban).datetime.hour/24+UTCDateTime(ban).datetime.minute/60/24)*np.pi*2 for iban,ban in enumerate(bans.keys())]
+        labels = [bans[iban] for iban in bans.keys()]
+        xticks = [xticks[i] for i,d in enumerate(rticks) if d>0]
+        labels = [labels[i] for i,d in enumerate(rticks) if d>0]
+        rticks = [d for d in rticks if d>0]
+        ax.set_rticks(rticks)
+        for x,r,l,c in zip(xticks,
+                           rticks,
+                           labels,
+                           range(len(labels))):
+            ax.plot(x,r,'o',
+                    label=l,
+                    color='C%d'%c,
+                    path_effects=[pe.withStroke(linewidth=5,
+                                                foreground='w'),
+                                  pe.withStroke(linewidth=3,
+                                                foreground='k')])
+    ax.set_yticklabels([])
+    ax.set_rorigin(max(radii[valid])/-2)
+    ax.text(np.pi,max(radii[valid])/-2,
+            data.index[0].strftime("%Y-%m-%d"),
+            ha='center',va='center')    
+    ax.set_xlabel(data.index[-1].strftime("%Y-%m-%d"))
+    plt.legend(loc='lower left',
+               bbox_to_anchor= (0.0, -0.2), 
+               ncol=2,
+               borderaxespad=0, 
+               frameon=False)
+    cb=plt.colorbar(s_m,orientation='horizontal')#,pad=0.07)
+    ticks = ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x*scale))
+    cb.ax.xaxis.set_major_formatter(ticks)
+    cb.ax.set_xlabel("Displacement (nm)")    
+    
+    ax.bar(theta[valid], radii[valid], 
+           color=s_m.to_rgba(data[valid]),
+           bottom=radii[valid]-1,
+           width=width)
+    
+    return ax
